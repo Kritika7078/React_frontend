e088fb6415f29f2c2b87f46ef8bb78c9d266455d
Kritika7078 <142921703+Kritika7078@users.noreply.github.com>
2024-07-03 09:40:02 +0530

    second

--- src/components/AllLogin.js
+++ src/components/AllLogin.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
@@ -15,41 +16,67 @@ export const AllLogin=()=>{
     const gotToNewPageSuperAdmin=()=>{
         navigate("/superadmin-login/superadmin");
       }
+
+      const [loginData,setLoginData]=useState(
+            {username:"",email:"",password:""}
+      )
+
+      function handleChange(event){
+            const {name,value}=event.target
+            setLoginData(prevData=>{
+                    return{...prevData,[name]:value}
+                }
+            )
+      }
+
+      function handleSubmit(e) {
+        e.preventDefault();
+        //submitToApi(formData)
+        console.log(loginData)
+      }
  
     return(
         <section className="AllLogin">
             <div className="AllLogin-content">
             <h1 className="AllLogin-heading">Log In</h1>
+            
+            <form onSubmit={handleSubmit}>
             <div id="AllLogin-element">
             <label htmlFor='username'>UserName :</label>
             <br></br>
-            <input id="username" type="text"></input>
+            <input id="username" onChange={handleChange} name="username" value={loginData.username} type="text"></input>
             </div>
             <div id="AllLogin-element">
-            <label htmlFor='username'>Email :</label>
+            <label htmlFor='email'>Email :</label>
             <br></br>
-            <input id="email" type="email"></input>
+            <input id="email" onChange={handleChange} name="email" value={loginData.email} type="email"></input>
             </div>
             <div id="AllLogin-element">
             <label htmlFor='password'>Password :</label>
             <br></br>
-            <input id="password" type="text"></input>
+            <input id="password" onChange={handleChange} name="password" value={loginData.password} type="text"></input>
             </div>
             <div id="AllLogin-element">
             {/* <input type='checkbox' id='remember'/> */}
             {/* <label htmlFor='remember' > Remember Me</label> */}
             </div>
-            <div id="AllLogin-element"><button type="submit" className="login-button" onClick={()=>{
+            <div id="AllLogin-element"><button type="submit" className="login-button" onClick={(e)=>{
                 if(path === '/user-login'){
+                    //handleSubmit(e)
                     gotToNewPageUser();
                 }
                 else if(path === '/admin-login'){
+                    //handleSubmit(e)
                     gotToNewPageAdmin();
                 } 
                 else if(path === '/superadmin-login'){
+                    //handleSubmit(e)
                     gotToNewPageSuperAdmin();
+                    
                 }
-            }}>Login</button></div>
+            }}>Login</button>
+            </div>
+            </form>
         </div> 
         </section>
     )
